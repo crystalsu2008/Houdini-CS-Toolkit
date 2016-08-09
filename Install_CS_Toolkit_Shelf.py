e68dbffdc40823ff6ca651c56d55246f291a9943
Chris Su <crystalsu2008@gmail.com>
2016-08-09 19:51:26 +0800

    Update shelf tool auto install procedure.

--- Install_CS_Toolkit_Shelf.py
+++ Install_CS_Toolkit_Shelf.py
@@ -2,23 +2,7 @@ import os
 import sys
 cstk_file_path = os.path.dirname(sys.argv[0])
 
-# hou.shelves.shelfSets() -- get all shelfSets list.
-# hou.shelves.shelves() -- get all shelves list.
-# hou.shelves.tools() -- get all tools list.
-
-# hou.shelves.loadFile('C:/Users/Administrator/Documents/houdini15.5/toolbar/csToolkit.shelf')
-sourceFile = cstk_file_path+'/csToolkit.shelf'
-targetFile = hou.hscriptExpression('$HOUDINI_USER_PREF_DIR')+'/toolbar/csToolkit.shelf'
-
-fsou = open(sourceFile, 'r')
-ftar = open(targetFile, 'w')
-
-for line in fsou:
-    print line
-
-ftar.close()
-fsou.close()
-'''
+## Install CS-Toolkit shelf.
 # Judge whether there is a 'inst_cs_toolkit' tool, if it exist then delete it.
 if hou.shelves.tool('inst_cs_toolkit'):
     hou.shelves.tools()['inst_cs_toolkit'].destroy()
@@ -27,28 +11,42 @@ if hou.shelves.tool('inst_cs_toolkit'):
 if 'cs_toolkit_shelf' in hou.shelves.shelves():
     hou.shelves.shelves()['cs_toolkit_shelf'].destroy()
 
-# Create a new shelf named 'cs_toolkit_shelf'.
-cstk_shelf = hou.shelves.newShelf(file_path='$HOUDINI_USER_PREF_DIR/toolbar/csToolkit.shelf', name='cs_toolkit_shelf', label='CS Toolkit')
+## Copy CS-Toolkit shelf file.
+# Get source and target file.
+sourceFile = cstk_file_path+'/csToolkit.shelf'
+targetFile = hou.hscriptExpression('$HOUDINI_USER_PREF_DIR')+'/toolbar/csToolkit.shelf'
 
-# Create a new tool name 'inst_cstk_tool'.
-inst_cstk_script = ("hou.hda.installFile('"+cstk_file_path+"/cs_simpleStar.otl')")
-inst_cstk_tool = hou.shelves.newTool(file_path='$HOUDINI_USER_PREF_DIR/toolbar/csToolkit.shelf', name='inst_cs_toolkit', label='Install CS Toolkit', script=inst_cstk_script)
+# Writhe the target file.
+fsou = open(sourceFile, 'r')
+ftar = open(targetFile, 'w')
 
-# Add 'inst_cstk_script' tool to 'cs_toolkit_shelf' shelf.
-tools = list(cstk_shelf.tools()) if len(cstk_shelf.tools()) else []
-tools.append(inst_cstk_tool)
-cstk_shelf.setTools(tools)
+part=()
+for line in fsou:
+    if line.find('houdini-cs-toolkit_path') != -1:
+        # Replace 'houdini-cs-toolkit_path' to specified file path.
+        part = line.partition('houdini-cs-toolkit_path')
+        line = part[0]+cstk_file_path+part[2]
+    ftar.write(line)
+
+ftar.close()
+fsou.close()
+
+# Install CS-Toolkit shelf frome shelf file.
+hou.shelves.loadFile(targetFile)
+
+# Get CS-Toolkit shelf instance.
+cstk_shelf = hou.shelves.shelves()['cs_toolkit_shelf']
 
 # Get 'shelf_set_1' instance.
-shelf_set1=hou.shelves.shelfSets()['shelf_set_1']
-# shelfSet1.shelves() -- get all shelves list under the 'shelf_set_1' shelfSet.
+shelf_set1 = hou.shelves.shelfSets()['shelf_set_1']
 
 # Add 'cs_toolkit_shelf' shelf to 'shelf_set_1'.
 new_shelves_list = list(shelf_set1.shelves())
 new_shelves_list.append(cstk_shelf)
 shelf_set1.setShelves(new_shelves_list)
 
-inst_cstk_tool.setFilePath('$HOUDINI_USER_PREF_DIR/toolbar/csToolkit.shelf')
-cstk_shelf.setFilePath('$HOUDINI_USER_PREF_DIR/toolbar/csToolkit.shelf')
-#HOUDINI_USER_PREF_DIR
-'''
+
+# hou.shelves.shelfSets() -- get all shelfSets list.
+# hou.shelves.shelves() -- get all shelves list.
+# hou.shelves.tools() -- get all tools list.
+# shelfSet1.shelves() -- get all shelves list under the 'shelf_set_1' shelfSet.
